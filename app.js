9c8ece6ff0adcbd5beb589ea820b2e45c14db430
Himanshu <pandeyrocks1210@gmail.com>
2023-12-02 16:16:11 +0530

    OAuth2.0

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import 'dotenv/config'
+import 'dotenv/config'
 // import { configDotenv } from "dotenv";
 // import encrypt from "mongoose-encryption";
 import bodyParser from "body-parser";
@@ -9,6 +9,8 @@ import mongoose from "mongoose"
 import session from "express-session";
 import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose"
+import {Strategy as GoogleStrategy} from "passport-google-oauth20"
+import findOrCreate from 'mongoose-findorcreate';
 
 const app=express();
 const port=3000;
@@ -30,10 +32,12 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema= new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 })
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 // const secret="Thisismysecret";
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
@@ -41,13 +45,53 @@ const User=new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get('/', (req, res) =>{
     res.render("home.ejs");
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 app.get('/register', (req, res) =>{
     res.render("register.ejs");
